bffb542703d9f3dcdf763d4c03538a0a5dfb1433
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-12-10 00:08:56 +0200

    More spaces!!

--- Perl/petooh.pl
+++ Perl/petooh.pl
@@ -11,42 +11,42 @@ my $level = 0;
 my $comment = 0;
 
 sub run {
-  for ($_[0]) {
-    when (/^Ko$/) { $result[$current_cell]++ }
-    when (/^kO$/) { $result[$current_cell]-- }
-    when (/^Kudah$/) { $current_cell++ }
-    when (/^kudah$/) { $current_cell-- }
-    when (/^Kukarek$/) { print chr $result[$current_cell] }
-    default { say "\nUnexpected pkokoblem" }
-  }
+    for ($_[0]) {
+        when (/^Ko$/) { $result[$current_cell]++ }
+        when (/^kO$/) { $result[$current_cell]-- }
+        when (/^Kudah$/) { $current_cell++ }
+        when (/^kudah$/) { $current_cell-- }
+        when (/^Kukarek$/) { print chr $result[$current_cell] }
+        default { say "\nUnexpected pkokoblem" }
+    }
 }
 
 sub cycle {
-  while ($result[$current_cell] > 0) {
-    run $_ for @{$stack{$level}}
-  }
+    while ($result[$current_cell] > 0) {
+        run $_ for @{$stack{$level}}
+    }
 }
 
 while (<>) {
-  while (1) {
-    if (/\G[\s!?.,:;()-]+/gc) { } # skip whitespace and punctuation
-    elsif (/\Gz+/gci) { $comment++ }
-    elsif ($comment and /\G.*?(morning)?/gc) { $comment-- if $1 }
-    elsif (/\G(Ko|kO|Kudah|kudah|Kukarek)/gc) {
-      if ($level > 0) {
-	push $stack{$level}, $1;
-      } else {
-	run $1;
-      }
+    while (1) {
+        if (/\G[\s!?.,:;()-]+/gc) { } # skip whitespace and punctuation
+        elsif (/\Gz+/gci) { $comment++ }
+        elsif ($comment and /\G.*?(morning)?/gc) { $comment-- if $1 }
+        elsif (/\G(Ko|kO|Kudah|kudah|Kukarek)/gc) {
+            if ($level > 0) {
+                push $stack{$level}, $1;
+            } else {
+                run $1;
+            }
+        }
+        elsif (/\GKud/gc) { $stack{++$level} = [] }
+        elsif (/\Gkud/gc) {
+            cycle();
+            $level--;
+        }
+        elsif (/\G(.)/gc) { say "\nChicktax errorek: $1" }
+        else { last }
     }
-    elsif (/\GKud/gc) { $stack{++$level} = [] }
-    elsif (/\Gkud/gc) {
-      cycle();
-      $level--;
-    }
-    elsif (/\G(.)/gc) { say "\nChicktax errorek: $1" }
-    else { last }
-  }
 }
 
 say '';
