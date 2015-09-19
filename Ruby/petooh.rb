520ec163f48c17c29ef31a529e974b5bb6b0c71d
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-09-20 00:36:20 +0500

    ...

--- Ruby/petooh.rb
+++ Ruby/petooh.rb
@@ -3,7 +3,7 @@
 # require 'petooh'
 # encoding: UTF-8
 
-# Generated from "lib/petooh.peg" on 2015-09-20 00:26:58 +0500
+# Generated from "lib/petooh.peg" on 2015-09-20 00:35:19 +0500
 
 # 
 module PETOOH
