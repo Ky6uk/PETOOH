5acf4a60e8ec9751612ef6edd1986f22d4ed1671
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2017-06-18 21:43:52 +0300

    Generate LLVM IR for loops

--- LLVM-based-compiler/src/main.cpp
+++ LLVM-based-compiler/src/main.cpp
@@ -156,6 +156,7 @@ std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
   auto *const i32 = builder.getInt32Ty();
   auto *const i64 = builder.getInt64Ty();
 
+  auto *const zeroI8 = llvm::ConstantInt::get(i8, 0);
   auto *const zeroI64 = llvm::ConstantInt::get(i64, 0);
 
   auto *const oneI8 = llvm::ConstantInt::get(i8, 1);
@@ -170,8 +171,7 @@ std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
 
   // Declare putchar function
   auto *const putcharType = llvm::FunctionType::get(i32, {i32}, false);
-  auto *const putcharFunc = module->getOrInsertFunction(
-      "putchar", putcharType);
+  auto *const putcharFunc = module->getOrInsertFunction("putchar", putcharType);
 
   // Create main function
   auto *const mainType = llvm::FunctionType::get(i32, false);
@@ -184,6 +184,7 @@ std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
   auto *const pos = builder.CreateAlloca(i64, nullptr, "pos");
   builder.CreateStore(zeroI64, pos);
 
+  std::vector<std::pair<llvm::BasicBlock *, llvm::BasicBlock *>> loopEnds;
   for (auto const instr : instrs) {
     switch (instr) {
     case Instr::INC_POS: {
@@ -213,10 +214,32 @@ std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
       auto *const intVal = builder.CreateIntCast(byteVal, i32, false);
       builder.CreateCall(putcharFunc, {intVal});
     } break;
-    case Instr::LOOP_START:
-      break;
-    case Instr::LOOP_END:
-      break;
+    case Instr::LOOP_START: {
+      auto *const start = llvm::BasicBlock::Create(ctx, "", mainFunc);
+      auto *const body = llvm::BasicBlock::Create(ctx, "", mainFunc);
+      loopEnds.emplace_back(start, body);
+
+      builder.CreateBr(start);
+
+      builder.SetInsertPoint(body);
+    } break;
+    case Instr::LOOP_END: {
+      auto *const start = loopEnds.back().first;
+      auto *const body = loopEnds.back().second;
+      auto *const end = llvm::BasicBlock::Create(ctx, "", mainFunc);
+      loopEnds.pop_back();
+
+      builder.CreateBr(start);
+
+      builder.SetInsertPoint(start);
+      auto *const posVal = builder.CreateLoad(pos);
+      auto *const byte = builder.CreateInBoundsGEP(bytes, {zeroI64, posVal});
+      auto *const byteVal = builder.CreateLoad(byte);
+      auto *const cond = builder.CreateICmpEQ(byteVal, zeroI8);
+      builder.CreateCondBr(cond, end, body);
+
+      builder.SetInsertPoint(end);
+    } break;
     }
   }
 
@@ -226,7 +249,13 @@ std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
 }
 
 int main(int argc, char *argv[]) {
-  InstrList instrs = {Instr::INC_BYTE, Instr::OUTPUT};
+  InstrTokenList const tokens(readInstructions(std::cin));
+  checkLoops(tokens);
+
+  InstrList instrs(tokens.size());
+  transform(tokens.begin(), tokens.end(), instrs.begin(),
+            [](InstrToken const &token) { return token.instr; });
+
   auto const module(createMainLlvmModule(30000, instrs));
   module->dump();
 
