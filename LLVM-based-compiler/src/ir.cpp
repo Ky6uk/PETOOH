3378042f9cae6933851f4a2e663ef946c7019ea5
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2017-06-18 23:45:58 +0300

    Refactor LLVM IR generation

--- LLVM-based-compiler/src/ir.cpp
+++ LLVM-based-compiler/src/ir.cpp
@@ -3,105 +3,118 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 
+namespace {
+llvm::LLVMContext LLVM_CTX;
+
+llvm::IntegerType *const I8 = llvm::Type::getInt8Ty(LLVM_CTX);
+llvm::IntegerType *const I32 = llvm::Type::getInt32Ty(LLVM_CTX);
+llvm::IntegerType *const I64 = llvm::Type::getInt64Ty(LLVM_CTX);
+
+llvm::ConstantInt *const ZERO_I8 = llvm::ConstantInt::get(I8, 0);
+llvm::ConstantInt *const ZERO_I64 = llvm::ConstantInt::get(I64, 0);
+llvm::ConstantInt *const ONE_I8 = llvm::ConstantInt::get(I8, 1);
+llvm::ConstantInt *const MINUS_ONE_I8 = llvm::ConstantInt::get(I8, -1);
+llvm::ConstantInt *const ONE_I64 = llvm::ConstantInt::get(I64, 1);
+llvm::ConstantInt *const MINUS_ONE_I64 = llvm::ConstantInt::get(I64, -1);
+
+llvm::GlobalVariable *allocateBytes(uint64_t const byteCount,
+                                    llvm::Module &module) {
+  auto *const arrayType = llvm::ArrayType::get(I8, byteCount);
+  auto *const initializer = llvm::ConstantAggregateZero::get(arrayType);
+  return new llvm::GlobalVariable(module, arrayType, false,
+                                  llvm::GlobalVariable::CommonLinkage,
+                                  initializer, "bytes");
+}
+
+llvm::Constant *declarePutchar(llvm::Module &module) {
+  auto *const putcharType = llvm::FunctionType::get(I32, {I32}, false);
+  return module.getOrInsertFunction("putchar", putcharType);
+}
+
+llvm::Function *createMain(llvm::Module &module) {
+  auto *const mainType = llvm::FunctionType::get(I32, false);
+  return llvm::Function::Create(mainType, llvm::Function::ExternalLinkage,
+                                "main", &module);
+}
+}
+
 std::unique_ptr<llvm::Module> createMainLlvmModule(uint64_t const byteCount,
                                                    InstrList const &instrs) {
-  llvm::LLVMContext &ctx = llvm::getGlobalContext();
-  auto module(std::make_unique<llvm::Module>("main", ctx));
-  llvm::IRBuilder<> builder(ctx);
+  auto module(std::make_unique<llvm::Module>("main", LLVM_CTX));
 
-  // Useful constants
-  auto *const i8 = builder.getInt8Ty();
-  auto *const i32 = builder.getInt32Ty();
-  auto *const i64 = builder.getInt64Ty();
+  // Globals
+  auto *const bytes = allocateBytes(byteCount, *module);
+  auto *const putcharFunc = declarePutchar(*module);
+  auto *const mainFunc = createMain(*module);
 
-  auto *const zeroI8 = llvm::ConstantInt::get(i8, 0);
-  auto *const zeroI64 = llvm::ConstantInt::get(i64, 0);
+  // Generate body of main function
+  llvm::IRBuilder<> builder(LLVM_CTX);
 
-  auto *const oneI8 = llvm::ConstantInt::get(i8, 1);
-  auto *const oneI64 = llvm::ConstantInt::get(i64, 1);
-
-  // Allocate program memory
-  auto *const arrayType = llvm::ArrayType::get(i8, byteCount);
-  auto *const initializer = llvm::ConstantAggregateZero::get(arrayType);
-  auto *const bytes = new llvm::GlobalVariable(
-      *module, arrayType, false, llvm::GlobalVariable::CommonLinkage,
-      initializer, "bytes");
-
-  // Declare putchar function
-  auto *const putcharType = llvm::FunctionType::get(i32, {i32}, false);
-  auto *const putcharFunc = module->getOrInsertFunction("putchar", putcharType);
-
-  // Create main function
-  auto *const mainType = llvm::FunctionType::get(i32, false);
-  auto *const mainFunc = llvm::Function::Create(
-      mainType, llvm::Function::ExternalLinkage, "main", module.get());
-  auto *const entry = llvm::BasicBlock::Create(ctx, "entry", mainFunc);
+  auto *const entry = llvm::BasicBlock::Create(LLVM_CTX, "entry", mainFunc);
   builder.SetInsertPoint(entry);
 
-  // Current position
-  auto *const pos = builder.CreateAlloca(i64, nullptr, "pos");
-  builder.CreateStore(zeroI64, pos);
+  auto *const pos = builder.CreateAlloca(I64, nullptr, "pos");
+  builder.CreateStore(ZERO_I64, pos);
 
-  std::vector<std::pair<llvm::BasicBlock *, llvm::BasicBlock *>> loopEnds;
+  auto const loadPos([&]() { return builder.CreateLoad(pos); });
+  auto const getByte([&]() {
+    return builder.CreateInBoundsGEP(bytes, {ZERO_I64, loadPos()});
+  });
+  auto const loadByte([&]() { return builder.CreateLoad(getByte()); });
+
+  auto const movePos([&](llvm::Value *const shift) {
+    builder.CreateStore(builder.CreateAdd(loadPos(), shift), pos);
+  });
+
+  auto const changeByte([&](llvm::Value *const inc) {
+    auto *const byte = getByte();
+    auto *const byteVal = builder.CreateLoad(byte);
+    builder.CreateStore(builder.CreateAdd(byteVal, inc), byte);
+  });
+
+  std::vector<std::pair<llvm::BasicBlock *, llvm::BasicBlock *>> loopStarts;
   for (auto const instr : instrs) {
     switch (instr) {
-    case Instr::INC_POS: {
-      auto *const posVal = builder.CreateLoad(pos);
-      builder.CreateStore(builder.CreateAdd(posVal, oneI64), pos);
-    } break;
-    case Instr::DEC_POS: {
-      auto *const posVal = builder.CreateLoad(pos);
-      builder.CreateStore(builder.CreateSub(posVal, oneI64), pos);
-    } break;
-    case Instr::INC_BYTE: {
-      auto *const posVal = builder.CreateLoad(pos);
-      auto *const byte = builder.CreateInBoundsGEP(bytes, {zeroI64, posVal});
-      auto *const byteVal = builder.CreateLoad(byte);
-      builder.CreateStore(builder.CreateAdd(byteVal, oneI8), byte);
-    } break;
-    case Instr::DEC_BYTE: {
-      auto *const posVal = builder.CreateLoad(pos);
-      auto *const byte = builder.CreateInBoundsGEP(bytes, {zeroI64, posVal});
-      auto *const byteVal = builder.CreateLoad(byte);
-      builder.CreateStore(builder.CreateSub(byteVal, oneI8), byte);
-    } break;
+    case Instr::INC_POS:
+      movePos(ONE_I64);
+      break;
+    case Instr::DEC_POS:
+      movePos(MINUS_ONE_I64);
+      break;
+    case Instr::INC_BYTE:
+      changeByte(ONE_I8);
+      break;
+    case Instr::DEC_BYTE:
+      changeByte(MINUS_ONE_I8);
+      break;
     case Instr::OUTPUT: {
-      auto *const posVal = builder.CreateLoad(pos);
-      auto *const byte = builder.CreateInBoundsGEP(bytes, {zeroI64, posVal});
-      auto *const byteVal = builder.CreateLoad(byte);
-      auto *const intVal = builder.CreateIntCast(byteVal, i32, false);
+      auto *const intVal = builder.CreateIntCast(loadByte(), I32, false);
       builder.CreateCall(putcharFunc, {intVal});
     } break;
     case Instr::LOOP_START: {
-      auto *const start = llvm::BasicBlock::Create(ctx, "", mainFunc);
-      auto *const body = llvm::BasicBlock::Create(ctx, "", mainFunc);
-      loopEnds.emplace_back(start, body);
+      auto *const start = llvm::BasicBlock::Create(LLVM_CTX, "", mainFunc);
+      auto *const body = llvm::BasicBlock::Create(LLVM_CTX, "", mainFunc);
+      loopStarts.emplace_back(start, body);
 
       builder.CreateBr(start);
-
       builder.SetInsertPoint(body);
     } break;
     case Instr::LOOP_END: {
-      auto *const start = loopEnds.back().first;
-      auto *const body = loopEnds.back().second;
-      auto *const end = llvm::BasicBlock::Create(ctx, "", mainFunc);
-      loopEnds.pop_back();
+      llvm::BasicBlock *const start = loopStarts.back().first;
+      llvm::BasicBlock *const body = loopStarts.back().second;
+      loopStarts.pop_back();
+      auto *const end = llvm::BasicBlock::Create(LLVM_CTX, "", mainFunc);
 
       builder.CreateBr(start);
-
       builder.SetInsertPoint(start);
-      auto *const posVal = builder.CreateLoad(pos);
-      auto *const byte = builder.CreateInBoundsGEP(bytes, {zeroI64, posVal});
-      auto *const byteVal = builder.CreateLoad(byte);
-      auto *const cond = builder.CreateICmpEQ(byteVal, zeroI8);
+      auto *const cond = builder.CreateICmpEQ(loadByte(), ZERO_I8);
       builder.CreateCondBr(cond, end, body);
-
       builder.SetInsertPoint(end);
     } break;
     }
   }
 
-  builder.CreateRet(llvm::ConstantInt::get(i32, 0));
+  builder.CreateRet(llvm::ConstantInt::get(I32, 0));
 
   return module;
 }
