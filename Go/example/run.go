364a025dc1dc824b047d6e877977148ced310ebc
Ilya Kaznacheev <dreamworm@ya.ru>
2018-10-22 03:19:41 +0300

    Gotooh example

---
+++ Go/example/run.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	petooh "https://github.com/ilyakaznacheev/PETOOH/Go"
+)
+
+func usage() {
+	fmt.Printf("Usage: %s filepath\n", os.Args[0])
+	os.Exit(0)
+}
+
+func init() {
+	flag.Usage = usage
+}
+
+func main() {
+	// parse parameters
+	flag.Parse()
+	if flag.NArg() == 0 {
+		usage()
+	}
+	filePath := flag.Arg(0)
+
+	// read file
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// process file
+	petooh.Process(file, os.Stdout)
+
+	fmt.Println()
+}
